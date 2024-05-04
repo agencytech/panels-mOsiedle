ba9a77347bcf44e38172321c19095cc1ebedea05
gugisek <gugisek@gmail.com>
2024-05-04 19:46:49 +0200

    baza panelmTablica

---
+++ components/panelmTablica/hero.php
@@ -0,0 +1,10 @@
+<div class="w-full h-screen">
+    <img src="public/img/test_mtablica.png" alt="" class="w-full h-full object-contain">
+</div>
+<a href="scripts/login/logout.php" class="text-gray-700 border-t border-[0.5] border-gray-100 group cursor-pointer duration-150 hover:bg-gray-100 flex items-center px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                            </svg>
+
+                            Wyloguj się
+                        </a>
