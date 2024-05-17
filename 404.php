b2fc40985fad40658948ae77520956968a25ef86
gugisek <gugisek@gmail.com>
2024-05-17 22:59:31 +0200

    fix 404

--- 404.php
+++ 404.php
@@ -9,8 +9,8 @@
                 <p class="mt-6 text-base leading-7 text-gray-600">Reasumując wszystkie aspekty kwintesencji tematu, dochodzimy do fundamentalnej konkluzji: <br/> Strona której szukasz jest niedostępna.</p>
                 <div class="mt-10 flex items-center justify-center gap-x-6">
                     <div class="sm:flex sm:flex-row-reverse">
-                        <button class="active:scale-95 duration-150 inline-flex w-full justify-center rounded-full bg-gray-900 duration-150 px-4 py-2 text-sm font-medium text-white shadow-sm hover:shadow-xl hover:bg-gray-500 sm:ml-2 sm:w-auto">Panel</button>
-                        <button onclick="popupUsersAddCloseConfirm()" type="button" class="active:scale-95 duration-150 mt-3 inline-flex w-full justify-center rounded-full px-4 py-2 text-sm font-medium text-gray-900 shadow-sm ring-inset ring-1 ring-[#3d3d3d] hover:ring-gray-500 hover:bg-gray-500 hover:text-white hover:shadow-xl duration-150 sm:mt-0 sm:w-auto">Strona główna</button>
+                        <a href="https://panel.mosiedle.pl/" class="active:scale-95 duration-150 inline-flex w-full justify-center rounded-full bg-gray-900 duration-150 px-4 py-2 text-sm font-medium text-white shadow-sm hover:shadow-xl hover:bg-gray-500 sm:ml-2 sm:w-auto">Panel</a>
+                        <a href="https://mosiedle.pl/" class="active:scale-95 duration-150 mt-3 inline-flex w-full justify-center rounded-full px-4 py-2 text-sm font-medium text-gray-900 shadow-sm ring-inset ring-1 ring-[#3d3d3d] hover:ring-gray-500 hover:bg-gray-500 hover:text-white hover:shadow-xl duration-150 sm:mt-0 sm:w-auto">Strona główna</a>
                     </div>
                 </div>
             </div>
